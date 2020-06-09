a560b9168b249a2b5c9c3a2407262ae6ebb06b39
Joe Baldwin <jcameronbaldwin@gmail.com>
2020-06-09 14:21:22 +0100

    Upload project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const spaceID = process.env.REACT_APP_SPACE;
+const token = process.env.REACT_APP_TOKEN;
+const url =
+  `https://cdn.contentful.com/spaces/${spaceID}/environments/master/entries?access_token=${token}`;
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios(url);
+      const links = response.data.items.map((link) => link.fields);
+      setData(links);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {data.map((link) => (
+          <li>
+            <a
+              href={link.url}
+              alt={link.title}
+              class={link.highlight ? "highlight" : null}
+            >
+              {link.title}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
